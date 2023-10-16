69c16b72ac2cf84b7faa28312e288dfe1cdd1dbe
sabs <dziedziochs@gmail.com>
2023-10-16 16:08:43 +0200

    Scoreboard added

--- button.py
+++ button.py
@@ -1,14 +1,14 @@
 import random
 from getkey import getkey, keys
-
+import datetime
 
 score = 0
 gameOver = False
 randomNum = random.randint(1, 100)
-failPercentage = 98
+failPercentage = 1
 
 print('\033[36;1m' + "THE BUTTON GAME" + '\033[m')
-print("\n • hit" + "\033[1m" , "ENTER" , '\033[m' + "to get 1 point \n • everytime you hit enter you also increase your odds to lose by 10% \n • to exit hit" + "\033[1m" , "ESCAPE" , '\033[m' + "\n ")
+print("\n • hit" + "\033[1m" , "ENTER" , '\033[m' + "to get 1 point \n • everytime you hit enter you also increase your odds to lose by 1% \n • to exit hit" + "\033[1m" , "ESCAPE" , '\033[m' + "\n ")
 
 def button():
     global score
@@ -16,16 +16,26 @@ def button():
     global randomNum
     global failPercentage
 
+    name = input('\33[36;1m' + "enter your name to proceed: " + '\033[m')
+
     while gameOver == False :
 
         if randomNum == failPercentage:
             gameOver = True
             print('\33[31;1m' + "GAME OVER \n" + '\033[m')
+            with open('scoreboard.txt', 'a') as file:
+                file.write(str(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")) + " " + name + ": " + str(score) + "\n")
+            with open('scoreboard.txt', 'r') as file:
+                print(file.read())
 
         elif failPercentage == 100 :
-            print('\033[36;1m' + "YOU WIN!" + '\033[m')
             gameOver = True
-            
+            print('\033[36;1m' + "YOU WIN!" + '\033[m')
+            with open('scoreboard.txt', 'a') as file:
+                file.write(str(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")) + " " + name + ": " + str(score) + "\n")
+            with open('scoreboard.txt', 'r') as file:
+                print(file.read())
+
         else:
             key = getkey("press enter to play")
 
@@ -39,6 +49,8 @@ def button():
             elif key == keys.ESC :
                 gameOver = True
                 print('\33[31;1m' + "GAME OVER \n" + '\033[m')
+                with open('scoreboard.txt', 'r') as file:
+                    print(file.read())
 
             else :
                 print("press enter to play or esc to exit")
