9d91c369cac0cc3cdffa49f9eda24aec89a91e64
sabs <dziedziochs@gmail.com>
2023-10-16 16:04:27 +0200

    Winner condition added

--- button.py
+++ button.py
@@ -5,9 +5,10 @@ from getkey import getkey, keys
 score = 0
 gameOver = False
 randomNum = random.randint(1, 100)
-failPercentage = 1
+failPercentage = 98
 
-print("THE BUTTON GAME")
+print('\033[36;1m' + "THE BUTTON GAME" + '\033[m')
+print("\n • hit" + "\033[1m" , "ENTER" , '\033[m' + "to get 1 point \n • everytime you hit enter you also increase your odds to lose by 10% \n • to exit hit" + "\033[1m" , "ESCAPE" , '\033[m' + "\n ")
 
 def button():
     global score
@@ -19,7 +20,12 @@ def button():
 
         if randomNum == failPercentage:
             gameOver = True
-            print("GAME OVER")
+            print('\33[31;1m' + "GAME OVER \n" + '\033[m')
+
+        elif failPercentage == 100 :
+            print('\033[36;1m' + "YOU WIN!" + '\033[m')
+            gameOver = True
+            
         else:
             key = getkey("press enter to play")
 
@@ -27,12 +33,12 @@ def button():
                 score += 1
                 if failPercentage < 100 :
                     failPercentage += 1
-                    randomNum = random.randint(1, 100)
-                    print("your score is:", score)
+                randomNum = random.randint(1, 100)
+                print("your score is:", score)
 
             elif key == keys.ESC :
                 gameOver = True
-                print("GAME OVER")
+                print('\33[31;1m' + "GAME OVER \n" + '\033[m')
 
             else :
                 print("press enter to play or esc to exit")
