80e1eb61167e58c4190d677358cf62fd592ea2eb
sabs <dziedziochs@gmail.com>
2023-10-16 15:40:35 +0200

    Game over condition added

--- button.py
+++ button.py
@@ -7,21 +7,28 @@ gameOver = False
 randomNum = random.randint(1, 100)
 failPercentage = 1
 
+print("THE BUTTON GAME")
 
 def button():
     global score
     global gameOver
     global randomNum
     global failPercentage
-    
+
     while gameOver == False :
-        key = getkey("press enter to play")
-        if key == keys.ENTER :
-            score += 1
-            if failPercentage < 100 :
-                failPercentage += 1
-                randomNum = random.randint(1, 100)
-                print("your score is:", score)  
+
+        if randomNum == failPercentage:
+            gameOver = True
+            print("GAME OVER")
+        else:
+            key = getkey("press enter to play")
+
+            if key == keys.ENTER :
+                score += 1
+                if failPercentage < 100 :
+                    failPercentage += 1
+                    randomNum = random.randint(1, 100)
+                    print("your score is:", score)  
 
 
 
